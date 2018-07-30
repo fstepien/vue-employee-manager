3705167ed9b3b013979db25460f9b58b79d2bf75
Filip Stepien <contact@filipstepien.com>
2018-07-29 21:11:32 -0400

    first components

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id='newemployee'>
+        <h3>New Employee</h3>
+    </div>
+</template>
+<script>
+export default {
+   name: 'newemployee',
+   data () {
+       return {
+           
+       }
+   } 
+}
+</script>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBdO3nkpzEIGmeGHjiajSdul-CairnPAoE",
+  authDomain: "employee-manager-853b4.firebaseapp.com",
+  databaseURL: "https://employee-manager-853b4.firebaseio.com",
+  projectId: "employee-manager-853b4",
+  storageBucket: "employee-manager-853b4.appspot.com",
+  messagingSenderId: "847005552383"
+};
